f900f48c3daff871969d9416b4dfc60133c56e12
PUSPOW <puspowmogor203@gmail.com>
2024-09-10 17:42:01 +0545

    change

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,7 +1,9 @@
 /** @format */
-
-export const baseUrl = "http://192.168.0.108:5000/api";
-export const imageUrl = "http://192.168.0.108:5000";
+export const baseUrl = "https://travel-backend-1-c4cf.onrender.com/api";
+export const imageUrl = "https://travel-backend-1-c4cf.onrender.com";
+// export const baseUrl = "http://192.168.0.108:5000/api";
+// export const imageUrl = "http://192.168.0.108:5000";
 export const userUrl = `${baseUrl}/users`;
+
 export const productUrl = `${baseUrl}/products`;
 export const orderUrl = `${baseUrl}/orders`;
