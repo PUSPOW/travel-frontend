17652338351c4d526756aa233ecb95db3a043696
PUSPOW <puspowmogor203@gmail.com>
2024-09-17 16:47:31 +0545

    popo

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -1,8 +1,8 @@
 /** @format */
-// export const baseUrl = "https://travel-backend-1-c4cf.onrender.com/api";
-// export const imageUrl = "https://travel-backend-1-c4cf.onrender.com";
-export const baseUrl = "http://192.168.0.105:5000/api";
-export const imageUrl = "http://192.168.0.105:5000";
+export const baseUrl = "https://travel-backend-1-c4cf.onrender.com/api";
+export const imageUrl = "https://travel-backend-1-c4cf.onrender.com";
+// export const baseUrl = "http://192.168.0.105:5000/api";
+// export const imageUrl = "http://192.168.0.105:5000";
 export const userUrl = `${baseUrl}/users`;
 
 export const productUrl = `${baseUrl}/products`;
